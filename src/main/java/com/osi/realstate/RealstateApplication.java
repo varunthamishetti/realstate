df3058682bd7d9e0cbd5f1efb738ce721b9ff6cc
Varun Thamishetti <vthamishetti@osius.com>
2023-08-23 21:19:21 +0530

    intial commit

---
+++ src/main/java/com/osi/realstate/RealstateApplication.java
@@ -0,0 +1,20 @@
+package com.osi.realstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RealstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealstateApplication.class, args);
+	}
+	 @GetMapping("/test")
+	public String index() {
+		return "Hey Guys! How are you doing Today!";
+	}
+
+}
---
+++ src/test/java/com/osi/realstate/RealstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osi.realstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
